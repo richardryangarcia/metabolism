81d71fa51f936b808ccebf7b5c174ff486d66a36
Richard Ryan <richardryangarcia@gmail.com>
2022-08-06 13:11:19 -0500

    add spinamp sdk

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import { zoraRequest } from "gql";
 import type { NextPage } from "next";
-import search from "gql/queries/search.gql";
 import { Grid } from "components/Grid/Grid";
 import { Page } from "components/Page/Page";
+import { fetchPlaylistById } from "@spinamp/spinamp-sdk";
 
 const Home: NextPage = () => {
   const handleRequest = async () => {
-    const res = await zoraRequest(search);
+    // const res = await zoraRequest(search);
+    // console.log(res);
+
+    const res = await fetchPlaylistById("UoFe4htiw5PXkA9SuMD7");
     console.log(res);
   };
 
